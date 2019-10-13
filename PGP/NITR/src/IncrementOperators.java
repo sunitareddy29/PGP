c8c2ed58a3c5614f30dad0124166c9001cc0677f
suniledureka <sunil.edureka@gmail.com>
2019-10-13 09:55:06 +0530

    removed

--- PGP/NITR/src/IncrementOperators.java
+++
@@ -1,9 +0,0 @@
-
-public class IncrementOperators {
-
-	public static void main(String[] args) {
-		int n = 10;
-		System.out.println(n++); //post_increment  10
-		System.out.println(++n); //pre_increment 12
-	}
-}
